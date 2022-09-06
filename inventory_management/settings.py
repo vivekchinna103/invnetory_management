0b05d7aa5a10b10132f6e17f58fccb38f6711756
vivekchinna103 <83503084+vivekchinna103@users.noreply.github.com>
2022-09-06 10:55:50 +0530

    Update settings.py

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'oa*3$-&_j%r0nwvlx(!ai349@wp=*v9+qj6b+kh&kbok=xuy4n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = FALSE
 
 ALLOWED_HOSTS = ["*"]
 
